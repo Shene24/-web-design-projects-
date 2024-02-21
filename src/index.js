5dc848a7550c1e0a9d8e2c82d92304cf2f172052
Nina2498 <shene1998m@gmail.com>
2024-02-21 20:30:41 +0300

    mongodb connection

--- src/index.js
+++ src/index.js
@@ -3,10 +3,47 @@ const app = express();
 const path = require("path");
 const hbs = require("hbs");
 const templatePath = path.join(__dirname, "../templates/views");
+const publicPath = path.join(__dirname, "../public");
+app.use(express.static(publicPath));
+const partialsPath = path.join(__dirname, "../templates/partials");
+hbs.registerPartials(partialsPath);
+const mongoose = require("mongoose");
 
 app.use(express.json());
 app.set("view engine", "hbs");
 app.set("views", templatePath);
+
+mongoose.connect("mongodb://localhost:27017/housebookingdb");
+var db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:")); // Fix this line
+db.once("open", function () {
+  console.log("Connected to database");
+});
+
+app.post("/index", async (req, res) => {
+  //  FIRSTname, lastnamae, email, phone, checkin, checkout, numberofguests, checkintime, checkouttime,
+  var firstname = req.body.firstname;
+  var lastname = req.body.lastname;
+  var email = req.body.email;
+  var phone = req.body.phone;
+  var checkin = req.body.checkin;
+  var checkout = req.body.checkout;
+  var numberofguests = req.body.numberofguests;
+  var checkintime = req.body.checkintime;
+  var checkouttime = req.body.checkouttime;
+  var numberofguests = req.body.numberofguests;
+  var reservation = new Reservation({
+    firstname: firstname,
+    lastname: lastname,
+    email: email,
+    phone: phone,
+    checkin: checkin,
+    checkout: checkout,
+    numberofguests: numberofguests,
+    checkintime: checkintime,
+    checkouttime: checkouttime,
+  });
+});
 
 app.get("/", (req, res) => {
   res.render("index");
